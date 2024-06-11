cffed7f870cfd86858c2ce433dc3117cca8e94e8
RoguePetz <briggsrhema@gmail.com>
2024-06-11 20:04:53 +0100

    fixed services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,20 +6,20 @@ import ai from "./assets/images/ai.png"
 
 function Services() {
     return (
-      <div className='flex flex-col mt-24 text-white justify-center items-center w-full'>
-        <p className=' text-4xl'>Services</p>
-        <div className=' flex gap-4'>
-            <div className=' flex flex-col items-center p-3 rounded-xl'>
-              <img src={game} width={70}/>
-              <p>Game Dev</p>
+      <div className='flex flex-col mt-24 text-white justify-center h-56 bg-neutral-800 items-center w-full'>
+        <p className=' text-4xl  bg-neutral-800'>Services</p>
+        <div className=' bg-neutral-800 flex gap-12'>
+            <div className=' bg-neutral-800 flex flex-col items-center  rounded-xl'>
+              <img className=' bg-neutral-800' src={game} width={70}/>
+              <p className=' bg-neutral-800' >Game Dev</p>
             </div>
-            <div className=' flex flex-col items-center p-3 rounded-xl'>
-              <img src={web}  width={70}/>
-              <p>web Dev</p>
+            <div className='  bg-neutral-800 flex flex-col items-center  rounded-xl'>
+              <img className=' bg-neutral-800' src={web}  width={70}/>
+              <p className=' bg-neutral-800' >web Dev</p>
             </div>
-            <div className=' flex flex-col items-center p-3  rounded-xl'>
-              <img src={ai}  width={70}/>
-              <p>AI Dev</p>
+            <div className=' bg-neutral-800 flex flex-col items-center  rounded-xl'>
+              <img className=' bg-neutral-800' src={ai}  width={70}/>
+              <p className=' bg-neutral-800' >AI Dev</p>
             </div>
         </div>
       </div>
